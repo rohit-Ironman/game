098f929338a41476ffd3bcaad95ac1eac3c04661
rohit-Ironman <72317546+rohit-Ironman@users.noreply.github.com>
2021-01-30 12:56:51 -0700

    Add files via upload

--- SupplyMission-master/sketch.js
+++ SupplyMission-master/sketch.js
@@ -6,13 +6,22 @@ var thunder, thunder1,thunder2,thunder3,thunder4;
 var engine, world; 
 var player; 
 var ground; 
+var obstacle1, obstacle2, obsacle3, obstacle4, obstacle5, obstacle6; 
 
 
 function setup(){
+
     engine = Engine.create();
 	world = engine.world; 
+
 	player = new Player(200, 300); 
-	ground = new Ground(300, 350, 25000, 20)
+	ground = new Ground(300, 350, 25000, 20); 
+	obstacle1 = new Obstacle(1000,300);
+	obstacle2 = new Obstacle(350,300);
+	obstacle3 = new Obstacle(540, 300); 
+	obstacle4 = new Obstacle(770,300);
+	obstacle5 = new Obstacle(1250,300);
+	obstacle6 = new Obstacle(1470,300);   
 }
 
 
@@ -22,5 +31,12 @@ function draw(){
 	createCanvas(1500,700);
 	background("black"); 
 	ground.display();
-player.display();
+	obstacle1.display();
+	player.display();
+	obstacle2.display();
+	obstacle3.display();
+	obstacle4.display(); 
+	obstacle5.display();
+	obstacle6.display();
+
 }
